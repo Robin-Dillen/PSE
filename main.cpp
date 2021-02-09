15d2a2fd600211fba690b6d16a3749ef78a5c1cf
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-02-09 20:45:44 +0100

    added google test to project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 int main() {
+    testing::InitGoogleTest();
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+    return RUN_ALL_TESTS();
 }
