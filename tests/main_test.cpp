be807b10d80d152c965e2d959dcd37916b9835c4
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-02-24 17:14:33 +0100

    changed google test

---
+++ tests/main_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by nibor on 24/02/2021.
+//
+
+#include <gtest/gtest.h>
+
+TEST(ExampleTests, testing) {
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
