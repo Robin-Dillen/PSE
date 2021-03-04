7747b6d3d398ad8fc88335f68f601228376333c1
Niels <nielsvandenbroeck11@gmail.com>
2021-03-04 15:46:25 +0100

    Parser

--- Parser.cpp
+++ Parser.cpp
@@ -9,79 +9,72 @@
 
 Parser::Parser(const string &filename) : _initCheck(this) {
     TiXmlDocument doc;
+    //Kijkt na of de file is ingeladen
     if (!doc.LoadFile(filename.c_str())) {
         std::cerr << doc.ErrorDesc() << std::endl;
         return;
     }
+    //Kijkt na of er een root aanwezig is
     TiXmlElement *root = doc.RootElement();
     if (root == NULL) {
         std::cerr << "Failed to load file: No root element." << std::endl;
         doc.Clear();
         return;
     }
-    unsigned int levering;
-    unsigned int interval;
-    unsigned int transport;
     vector<VaccinatieCentrum*> vaccinatieCentra;
+    //Loop over alle kinderen van root
     for(TiXmlElement* firstElement = root->FirstChildElement(); firstElement != NULL; firstElement = firstElement->NextSiblingElement()) {
         string firstName = firstElement->Value();
-        if (firstName == "HUB") {
-            for(TiXmlElement* secondElement = firstElement->FirstChildElement(); secondElement != NULL; secondElement = secondElement->NextSiblingElement()) {
-                string secondName = secondElement->Value();
-                if(secondElement->GetText() != NULL) {
-                    string s = secondElement->GetText();
-                    stringstream g(s);
-                    if(secondName == "levering") {
-                        g >> levering;
-                    }
-                    else if(secondName == "interval") {
-                        g >> interval;
-                    }
-                    else if(secondName == "transport") {
-                        g >> transport;
-                    }
-                }
-                /*if (secondname == "CENTRA") {
-                    vector<VaccinatieCentrum*> VCentra;
-                    for (TiXmlElement *thirdelem = secondelem->FirstChildElement(); thirdelem != NULL; thirdelem = thirdelem->NextSiblingElement()) {
+        //Vacinatiecentrum aanmaken
+        if(firstName == "VACCINATIECENTRUM") {
+            unsigned int inwoners = 0;
+            unsigned int capaciteit = 0;
+            //Loop over alle atributen van Vaccinatiecentrum
+            string naam = firstElement->FirstChildElement("naam")->GetText();
+            string adres = firstElement->FirstChildElement("adres")->GetText();
 
-                    }
-                }
-                */
-            }
-        }
-        else if(firstName == "VACCINATIECENTRUM") {
-            string naam;
-            string adres;
-            unsigned int inwoners;
-            unsigned int capaciteit;
-            for(TiXmlElement* secondElement = firstElement->FirstChildElement(); secondElement != NULL; secondElement = secondElement->NextSiblingElement()) {
-                string secondName = secondElement->Value();
-                string tekst = secondElement->GetText();
-                if(secondElement->GetText() != NULL) {
-                    string s = secondElement->GetText();
-                    stringstream g(s);
-                    if(secondName == "naam") {
-                        naam = s;
-                    } else if (secondName == "adres") {
-                        adres = s;
-                    } else if (secondName == "inwoners") {
-                        g >> inwoners;
-                    } else if (secondName == "capaciteit") {
-                        g >> capaciteit;
-                    }
-                }
-            }
-            ENSURE(naam != "" && adres != "" && inwoners != 0 && capaciteit != 0, "fout bij het parsen!");
+            stringstream g(firstElement->FirstChildElement("inwoners")->GetText());
+            g >> inwoners;
+
+            stringstream h(firstElement->FirstChildElement("capaciteit")->GetText());
+            h >> capaciteit;
+
+            //variabelen moeten een waarde hebben gekregen
+            ENSURE(naam != "" && adres != "" && inwoners != 0 && capaciteit != 0, "Sommige variabelen van VACCINATIECENTRUM werden niet correct meegegeven.");
             VaccinatieCentrum *V = new VaccinatieCentrum(capaciteit, inwoners, naam, adres);
             vaccinatieCentra.push_back(V);
         }
     }
-    ENSURE(levering != 0 && interval != 0 && transport != 0 , "fout bij het parsen!");
-    Hub* H = new Hub(levering,interval,transport);
-    H->setFverbondenCentra(vaccinatieCentra);
-    ENSURE(isProperlyInitialized(), "constructor must end in properlyInitialized state");
-    fhub = H;
+    ENSURE(vaccinatieCentra.size() != 0, "Er is geen vaccinatiecentrum gegeven.");
+    //Hub aanmaken
+    if(root->FirstChildElement("HUB")){
+        TiXmlElement* hub = root->FirstChildElement("HUB");
+        unsigned int levering = 0;
+        unsigned int interval = 0;
+        unsigned int transport = 0;
+        stringstream g(hub->FirstChildElement("levering")->GetText());
+        g >> levering;
+
+        stringstream h(hub->FirstChildElement("interval")->GetText());
+        h >> interval;
+
+        stringstream i(hub->FirstChildElement("transport")->GetText());
+        i >> transport;
+
+        //variabelen moeten een waarde hebben gekregen
+        ENSURE(levering != 0 && interval != 0 && transport != 0 , "Sommige variabelen van HUB werden niet correct meegegeven.");
+        Hub* H = new Hub(levering,interval,transport);
+        H->setFverbondenCentra(vaccinatieCentra);
+        fhub = H;
+        ENSURE(isProperlyInitialized(), "constructor must end in properlyInitialized state");
+    }
+    //Hub moet aanwezig zijn
+    else{
+        std::cerr << "Failed to load file: No HUB element." << std::endl;
+        doc.Clear();
+        return;
+    }
+
 }
 
 Hub *Parser::getFhub() const {
