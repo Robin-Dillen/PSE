d11b48229ff4d2a86d20b9498dffece5d1b9b112
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-03-04 14:02:24 +0100

    added tests file

---
+++ VaccinSimulatieTests.cpp
@@ -0,0 +1,58 @@
+//
+// Created by nibor on 2/03/2021.
+//
+
+#include "gtest/gtest.h"
+#include "Hub.h"
+#include "VaccinatieCentrum.h"
+#include "Parser.h"
+
+class VaccinSimulatieTest : public ::testing::Test {
+public:
+    VaccinSimulatieTest() {
+        P = new Parser("../XMLfiles/vb.xml");
+        H = P->getFhub();
+    }
+
+protected:
+    friend class Hub;
+    // You should make the members protected s.t. they can be
+    // accessed from sub-classes.
+
+    friend class VaccinatieCentrum;
+    // You should make the members protected s.t. they can be
+    // accessed from sub-classes.
+
+/**
+virtual void SetUp() will be called before each test is run.  You
+should define it if you need to initialize the variables.
+Otherwise, this can be skipped.
+*/
+    virtual void SetUp() {
+    }
+
+/**
+virtual void TearDown() will be called after each test is run.
+You should define it if there is cleanup work to do.  Otherwise,
+you don't have to provide it.
+*/
+    virtual void TearDown() {
+    }
+
+/**
+Declares the variables your tests want to use.
+*/
+    Hub *H;
+    Parser *P;
+};
+
+
+TEST_F(VaccinSimulatieTest, DefaultConstructor) {
+    EXPECT_TRUE(H->isProperlyInitialized());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
