4ed72d77146630fa05f8a64829d14309ebd5a8a8
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-03-06 14:36:39 +0100

    change location of output file

--- Output.cpp
+++ Output.cpp
@@ -8,21 +8,25 @@
 
 using namespace std;
 
-Output::Output(Hub* h, int y, int m, int w) {
-    makeOutputFile(h,y,m,w);
+Output::Output(Hub *h, int y, int m, int w) {
+    makeOutputFile(h, y, m, w);
 }
 
-void Output::makeOutputFile(Hub* h, int y, int m, int w) {
-    ofstream outputFile("Output.txt");
-    outputFile<<"Overzicht van vaccinaties na: "<<y<<" jaren, "<<m<<" maanden en "<<w<<" weken.\n\n";
-    outputFile<<"Hub ("<<h->getKaantalVaccinsPerLevering() <<" vaccins)\n";
+void Output::makeOutputFile(Hub *h, int y, int m, int w) {
+    ofstream outputFile("../Output.txt");
+    outputFile << "Overzicht van vaccinaties na: " << y << " jaren, " << m << " maanden en " << w << " weken.\n\n";
+    outputFile << "Hub (" << h->getKaantalVaccinsPerLevering() << " vaccins)\n";
 
-    for(map<string, VaccinatieCentrum *>::const_iterator it = h->getFverbondenCentra().begin(); it!= h->getFverbondenCentra().end(); it++){
-        outputFile<<"\t-> "<<it->first<<" ("<< it->second->getAantalVaccinaties()<<" vaccins)\n";
+    for (map<string, VaccinatieCentrum *>::const_iterator it = h->getFverbondenCentra().begin();
+         it != h->getFverbondenCentra().end(); it++) {
+        outputFile << "\t-> " << it->first << " (" << it->second->getAantalVaccinaties() << " vaccins)\n";
     }
-    outputFile<<"\n";
-    for(map<string, VaccinatieCentrum *>::const_iterator it = h->getFverbondenCentra().begin(); it!= h->getFverbondenCentra().end(); it++){
-        outputFile<<it->first<<": "<< it->second->getAantalVaccinaties()<<" gevacineerd, nog "<< it->second->getKaantalInwoners()-it->second->getAantalVaccinaties()<<" inwoners niet gevaccineerd.\n";
+    outputFile << "\n";
+    for (map<string, VaccinatieCentrum *>::const_iterator it = h->getFverbondenCentra().begin();
+         it != h->getFverbondenCentra().end(); it++) {
+        outputFile << it->first << ": " << it->second->getAantalVaccinaties() << " gevacineerd, nog "
+                   << it->second->getKaantalInwoners() - it->second->getAantalVaccinaties()
+                   << " inwoners niet gevaccineerd.\n";
     }
     outputFile.close();
 }
