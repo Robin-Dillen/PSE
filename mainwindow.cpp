524bc8a93f32bfd5bc34d3f54be9d8ac55dc02a3
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-06-03 17:44:39 +0200

    working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,11 +3,26 @@
 #include "VaccinSimulatie.h"
 #include<iostream>
 
+#include <QtCharts>
+#include <QChartView>
+#include <QPieSeries>
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+        QMainWindow(parent),
+        ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    QPieSeries *series = new QPieSeries();
+    series->append("Niet gevaccineert", 100);
+    series->append("half gevaccineert", 0);
+    series->append("volledig gevaccineert", 0);
+
+    QChart *chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Test");
+
+    QChartView *view = new QChartView(chart);
+    view->setParent(ui->horizontalFrame_page1);
 }
 
 void MainWindow::setSimulation(VaccinSimulatie* s){
